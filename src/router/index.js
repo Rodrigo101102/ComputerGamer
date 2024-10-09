dbd473a548493d457d851ba7c33461064340df1d
melisa176 <carolinacastillon27@gmail.com>
2024-10-09 17:08:45 -0500

    Realizar home

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,21 @@
 import { Router } from "express"
 const router= Router()
 
-router.get('/', (req, res) => res.render('index',{title: 'Computer Gamer'}))
+router.get('/', (req, res) => {
+    // Array de imágenes
+    const images = [
+        { src: 'img/image1.png', alt: 'Imagen 1' },
+        { src: 'img/image2.png', alt: 'Imagen 2' },
+        { src: 'img/image3.png', alt: 'Imagen 3' }
+    ];
+
+    // Renderizamos la vista y enviamos las imágenes
+    res.render('index', {
+        title: 'Computer Gamer',
+        images: images
+    });
+})
+
 router.get('/about', (req, res) => res.render('about',{title:'Sobre Nosotros'}))
 router.get('/contact', (req, res) => res.render('contact',{title: 'Contáctanos'}))
 router.get('/catalogo',(req,res)=>res.render('catalogo',{title: 'Catalogo'}))
