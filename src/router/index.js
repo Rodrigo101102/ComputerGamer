f114458aeb057672c290e2f91e6aaf2506d3d401
Rodrigo101102 <rodrigo050318@hotmail.com>
2024-10-09 15:53:41 -0500

    primer_cambio

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import { Router } from "express"
 const router= Router()
 
-router.get('/', (req, res) => res.render('index',{title: 'Mi primer proyecto en NodeJS'}))
+router.get('/', (req, res) => res.render('index',{title: 'Computer Gamer'}))
 router.get('/about', (req, res) => res.render('about',{title:'Sobre Nosotros'}))
 router.get('/contact', (req, res) => res.render('contact',{title: 'Contáctanos'}))
-router.get('/catalogo',(req,res)=>res.render('catalogo',{title: 'Este es mi catalogo'}))
+router.get('/catalogo',(req,res)=>res.render('catalogo',{title: 'Catalogo'}))
 
 export default router
